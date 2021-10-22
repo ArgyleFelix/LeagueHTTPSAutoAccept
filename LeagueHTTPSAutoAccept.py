99c9fce315d305a37d3001cdf4b283988ce6ead3
ArgyleFelix <slackoben@gmail.com>
2021-10-22 18:59:16 +0200

    Updated script. Thanks to toxamin.

--- LeagueHTTPSAutoAccept.py
+++ LeagueHTTPSAutoAccept.py
@@ -2,6 +2,8 @@ import subprocess
 import re
 import os
 import time
+import json
+import math
 from termcolor import colored
 try:
     import requests
@@ -33,7 +35,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 session = requests.session()
 session.verify = False
 
-print(colored("Auto Accept is Currently Offline.", "red"))
+print(colored("Auto Accept is currently offline.", "red"))
 print('Press "Ctrl + C" to stop the script.')
 print('Type "start" to start the Auto Accept.\n')
 
@@ -41,17 +43,26 @@ def DoAccept(ready):
     try:
         if ready == "start":
             os.system("cls" if os.name == "nt" else "clear")
-            print(colored("Auto Accept is Currently Active.", "green"))
+            print(colored("Auto Accept is currently active.", "green"))
             print('Press "Ctrl + C" to stop the script.\n')
 
-            while True:
-                session.post('https://127.0.0.1:%s/lol-matchmaking/v1/ready-check/accept' %
-                        port, data={}, auth=requests.auth.HTTPBasicAuth('riot', password))
-                time.sleep(1.00)
+            t = time.localtime()
+            current_time = time.strftime("%H:%M:", t)
 
+            while True:
+                checkResponse = session.get('https://127.0.0.1:%s/lol-matchmaking/v1/ready-check' %
+                        port, data={}, auth=requests.auth.HTTPBasicAuth('riot', password))
+                if math.floor(checkResponse.status_code / 100) == 2:
+                    jsonResponse = json.loads(checkResponse.text)
+                    if jsonResponse["state"] == "InProgress" and jsonResponse["playerResponse"] != "Accepted":
+                        acceptResponse = session.post('https://127.0.0.1:%s/lol-matchmaking/v1/ready-check/accept' %
+                                port, data={}, auth=requests.auth.HTTPBasicAuth('riot', password))
+                        if math.floor(acceptResponse.status_code / 100) == 2:
+                            print(current_time, "Match accepted!")
+                time.sleep(1.00)
         else:
             os.system("cls" if os.name == "nt" else "clear")
-            print("Unknown Command:", ready, "\n")
+            print("Unknown command:", ready, "\n")
             print('Type "start" to start the Auto Accept.')
             print('Press "Ctrl + C" to stop the script.\n')
             NewInput = input()
@@ -59,7 +70,7 @@ def DoAccept(ready):
 
     except KeyboardInterrupt:
         os.system("cls" if os.name == "nt" else "clear")
-        print(colored("Auto Accept has been Closed.\n", "red"))
+        print(colored("Auto Accept has been closed.\n", "red"))
         end_time = time.time()
         time_lapsed = end_time - start_time
         time_converter(time_lapsed)
