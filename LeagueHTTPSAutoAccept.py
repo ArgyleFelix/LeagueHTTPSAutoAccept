85fb1c3bfe712d7d7bd2baecce1a447459048a23
ArgyleFelix <slackoben@gmail.com>
2021-11-25 18:54:59 +0100

    Cleared up code

--- LeagueHTTPSAutoAccept.py
+++ LeagueHTTPSAutoAccept.py
@@ -47,12 +47,12 @@ def countdown(t):
     while t:
         time.sleep(1)
         t -= 1
-        os.system("cls" if os.name == "nt" else "clear")
+        ClearConsole()
         if t == 1:
             print(colored("League found.", "green"), "Please wait", colored(t, "yellow"), "second.")
         else:
             print(colored("League found.", "green"), "Please wait", colored(t, "yellow"), "seconds.")
-    PrintActiveNotice(True)
+    PrintActiveNotice(False)
 
 def TimeConverter(sec):
     mins = sec // 60
